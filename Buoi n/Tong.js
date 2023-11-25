5953f41040e5f0faf8ba5cd3216060d654b1f2de
HongAnh <honganh068204@gmail.com>
2023-11-25 22:51:04 +0700

    first commit

---
+++ Buoi n/Tong.js
@@ -0,0 +1,7 @@
+
+
+function tong(){
+    var so1 = parseFloat(document.getElementById("so1").value);
+    var so2 = parseFloat(document.getElementById("so2").value); 
+    document.getElementById("kp").innerHTML = "Tong la " + (so1 + so2); 
+}
